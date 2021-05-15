f2400d134d54416b9a65050dfb97825d35de5695
chylex <contact@chylex.com>
2021-05-15 19:46:51 +0200

    Update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,19 @@
-import org.jetbrains.changelog.closure
-import org.jetbrains.intellij.tasks.PatchPluginXmlTask
-import org.jetbrains.intellij.tasks.PublishTask
-import org.jetbrains.intellij.tasks.RunIdeTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   idea apply true
-  kotlin("jvm") version "1.3.72"
+  kotlin("jvm") version "1.5.0"
   id("org.jetbrains.intellij") version "0.7.2"
-  id("org.jetbrains.changelog") version "1.1.2"
-  id("com.github.ben-manes.versions") version "0.38.0"
 }
 
 tasks {
   withType<KotlinCompile> {
     kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
   }
-
-  named<Zip>("buildPlugin") {
-    dependsOn("test")
-    archiveFileName.set("AceJump.zip")
-  }
-
-  withType<RunIdeTask> {
-    dependsOn("test")
-    findProperty("luginDev")?.let { args = listOf(projectDir.absolutePath) }
-  }
-
-  withType<PublishTask> {
-    val intellijPublishToken: String? by project
-    token(intellijPublishToken)
-  }
-
-  withType<PatchPluginXmlTask> {
-    sinceBuild("201.6668.0")
-    changeNotes({ changelog.getLatest().toHTML() })
-  }
-}
-
-changelog {
-  path = "${project.projectDir}/CHANGES.md"
-  header = closure { "${project.version}" }
 }
 
 dependencies {
-  // gradle-intellij-plugin doesn't attach sources properly for Kotlin :(
   compileOnly(kotlin("stdlib-jdk8"))
-  // https://github.com/promeG/TinyPinyin
-  implementation("com.github.promeg:tinypinyin:2.0.3")
 }
 
 repositories {
@@ -56,10 +22,10 @@ repositories {
 }
 
 intellij {
-  version = "2020.2"
+  version = "2021.1"
   pluginName = "AceJump"
   updateSinceUntilBuild = false
-  setPlugins("java")
+  setPlugins("java", "IdeaVIM:0.66")
 }
 
 group = "org.acejump"
