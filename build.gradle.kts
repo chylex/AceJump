a6381a2a3481613f7de1be1f40c6fa59aa6d1d60
chylex <contact@chylex.com>
2023-12-12 18:42:34 +0100

    Update Gradle to 8.5 and IntelliJ to 2023.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,41 +3,49 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.8.0"
-	id("org.jetbrains.intellij") version "1.15.0"
+  kotlin("jvm") version "1.9.10"
+  id("org.jetbrains.intellij") version "1.16.1"
 }
 
 group = "org.acejump"
 version = "chylex-14"
 
 repositories {
-	mavenCentral()
-}
-
-kotlin {
-	jvmToolchain(17)
+  mavenCentral()
 }
 
 intellij {
-	version.set("2023.2")
-	updateSinceUntilBuild.set(false)
-	plugins.add("IdeaVIM:chylex-20")
-	
-	pluginsRepositories {
-		custom("https://intellij.chylex.com")
-	}
+  version.set("2023.3")
+  updateSinceUntilBuild.set(false)
+  plugins.add("IdeaVIM:chylex-22")
+  
+  pluginsRepositories {
+    custom("https://intellij.chylex.com")
+  }
+}
+
+kotlin {
+  jvmToolchain(17)
+}
+
+dependencies {
+  testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
 
 tasks.patchPluginXml {
-	sinceBuild.set("231")
+  sinceBuild.set("233")
 }
 
 tasks.buildSearchableOptions {
-	enabled = false
+  enabled = false
+}
+
+tasks.test {
+  useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions.freeCompilerArgs = listOf(
-		"-Xjvm-default=all"
-	)
+  kotlinOptions.freeCompilerArgs = listOf(
+    "-Xjvm-default=all"
+  )
 }
