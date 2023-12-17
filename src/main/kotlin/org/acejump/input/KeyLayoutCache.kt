2681d9901f5f5d3d1b50c4588ba07abef0330962
chylex <contact@chylex.com>
2023-12-18 00:45:28 +0100

    Change priority order of two-character tags

--- src/main/kotlin/org/acejump/input/KeyLayoutCache.kt
+++ src/main/kotlin/org/acejump/input/KeyLayoutCache.kt
@@ -8,13 +8,6 @@ import org.acejump.config.AceSettings
  */
 internal object KeyLayoutCache {
   /**
-   * Sorts tags according to current keyboard layout settings, and some predefined rules that force tags with digits, and tags with two
-   * keys far apart, to be sorted after other (easier to type) tags.
-   */
-  lateinit var tagOrder: Comparator<String>
-    private set
-  
-  /**
    * Returns all possible two key tags, pre-sorted according to [tagOrder].
    */
   lateinit var allPossibleTagsLowercase: List<String>
@@ -24,7 +17,7 @@ internal object KeyLayoutCache {
    * Called before any lazily initialized properties are used, to ensure that they are initialized even if the settings are missing.
    */
   fun ensureInitialized(settings: AceSettings) {
-    if (!::tagOrder.isInitialized) {
+    if (!::allPossibleTagsLowercase.isInitialized) {
       reset(settings)
     }
   }
@@ -33,14 +26,15 @@ internal object KeyLayoutCache {
    * Re-initializes cached data according to updated settings.
    */
   fun reset(settings: AceSettings) {
-    tagOrder = compareBy(
-      String::length,
-      settings.layout.priority(String::last)
-    )
-    
     @Suppress("ConvertLambdaToReference")
     val allSuffixChars = processCharList(settings.allowedChars).ifEmpty { processCharList(settings.layout.allChars).toList() }
     val allPrefixChars = processCharList(settings.prefixChars).filterNot(allSuffixChars::contains).plus("")
+    
+    val tagOrder = compareBy(
+      String::length,
+      { if (it.length == 1) Int.MIN_VALUE else allPrefixChars.indexOf(it.first().toString()) },
+      settings.layout.priority(String::last)
+    )
     
     allPossibleTagsLowercase = allSuffixChars
       .flatMap { suffix -> allPrefixChars.map { prefix -> "$prefix$suffix" } }
