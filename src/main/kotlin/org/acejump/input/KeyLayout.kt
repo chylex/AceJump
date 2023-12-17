92dcd033fb55ed30864d609efd3f1c558231e9cc
chylex <contact@chylex.com>
2023-12-17 21:49:24 +0100

    Make priority of unknown characters lower than known characters

--- src/main/kotlin/org/acejump/input/KeyLayout.kt
+++ src/main/kotlin/org/acejump/input/KeyLayout.kt
@@ -18,7 +18,7 @@ enum class KeyLayout(internal val rows: Array<String>, priority: String) {
   internal val allChars = rows.joinToString("").toCharArray().apply(CharArray::sort).joinToString("")
   internal val allPriorities = priority.mapIndexed { index, char -> char to index }.toMap()
   
-  internal inline fun priority(crossinline tagToChar: (String) -> Char): (String) -> Int? {
-    return { allPriorities[tagToChar(it)] }
+  internal inline fun priority(crossinline tagToChar: (String) -> Char): (String) -> Int {
+    return { allPriorities.getOrDefault(tagToChar(it), Int.MAX_VALUE) }
   }
 }
